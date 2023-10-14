17d30628970f2968de50ff5c0daa7c443cd2f61d
pine <2277367263@qq.com>
2023-10-14 16:44:32 +0800

    feat: 实时输出词法分析结果
    每次fprintf之后都调用fflush刷新缓冲区
    使其可以在交互模式下实时查看词法分析的结果

--- main.c
+++ main.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include "y.tab.h"
 
-FILE *out_file;
+FILE *lex_out;
+const char *lex_out_path = "./lex.out";
 
 extern FILE *yyin;
 extern int yyparse();
 
 void output(enum yytokentype type, const char *typename, const char *text)
 {
-    
     switch (type)
     {
     case ID:
-        fprintf(out_file, "%s(%d, \"%s\")\n", typename, type, text);
+        fprintf(lex_out, "%s(%d, \"%s\")\n", typename, type, text);
         break;
     default:
-        fprintf(out_file, "%s(%d, %s)\n", typename, type, text);
+        fprintf(lex_out, "%s(%d, %s)\n", typename, type, text);
         break;
     }
+    fflush(lex_out);
 }
 
 int main(int argc, char **argv)
 {
     yyin = fopen("./test.txt", "r");
-    out_file = fopen("./lex.out", "w");
+    lex_out = fopen(lex_out_path, "w");
     yyparse();
 
+    fclose(lex_out);
     puts("\ndone");
 }
 
