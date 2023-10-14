91d75afcbbb9a7824a58eafa1a646953409c67d4
pine <2277367263@qq.com>
2023-10-14 15:52:51 +0800

    feat: 重新添加词法分析结果输出功能
    和原先的RET(x)进行了融合
    可以更灵活地进行功能开关切换

--- main.c
+++ main.c
@@ -6,10 +6,24 @@ FILE *out_file;
 extern FILE *yyin;
 extern int yyparse();
 
+void output(enum yytokentype type, const char *typename, const char *text)
+{
+    
+    switch (type)
+    {
+    case ID:
+        fprintf(out_file, "%s(%d, \"%s\")\n", typename, type, text);
+        break;
+    default:
+        fprintf(out_file, "%s(%d, %s)\n", typename, type, text);
+        break;
+    }
+}
+
 int main(int argc, char **argv)
 {
     yyin = fopen("./test.txt", "r");
-    // out_file = fopen("./output.txt", "w");
+    out_file = fopen("./lex.out", "w");
     yyparse();
 
     puts("\ndone");
@@ -17,6 +31,6 @@ int main(int argc, char **argv)
 
 int yyerror(char *s)
 {
-    puts(s);
+    printf("yyerror: %s\n", s);
     return 1;
 }
